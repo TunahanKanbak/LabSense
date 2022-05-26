58e141186990446bc5f3136df94d235b2e7aa910
Tunahan Kanbak <kanbaktunahan@gmail.com>
2022-05-26 23:42:17 +0300

    html table example

--- test.py
+++ test.py
@@ -1,51 +1,39 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-from dash import Dash, dcc, html
-import plotly.express as px
+from dash import Dash, html
 import pandas as pd
+
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+
+table = {
+    'border-collapse': 'collapse',
+    'border': '2px solid rgb(200, 200, 200)',
+    'letter-spacing': '1px',
+    'font-family': 'sans-serif',
+    'font-size': '.8rem'
+}
+
+cell = {
+    'border': '1px solid rgb(190, 190, 190)',
+    'padding': '5px 10px'
+}
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col], style=cell) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ], style=table)
+
 
 app = Dash(__name__)
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
-)
-
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Hello Dash',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
-
-    html.Div(children='Dash: A web application framework for your data.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
-
-    dcc.Graph(
-        id='example-graph-2',
-        figure=fig
-    )
+app.layout = html.Div([
+    html.H4(children='US Agriculture Exports (2011)'),
+    generate_table(df)
 ])
 
 if __name__ == '__main__':
