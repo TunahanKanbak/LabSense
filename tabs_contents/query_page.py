8ef20d4209603c18fde5710b5e3e8ca37235a44f
Tunahan Kanbak <kanbaktunahan@gmail.com>
2022-06-22 23:29:09 +0300

    result dosyasinin adi formatlandi

--- tabs_contents/query_page.py
+++ tabs_contents/query_page.py
@@ -8,6 +8,7 @@ import numpy as np
 import dash_bootstrap_components as dbc
 from app import app
 from database import labDBmanager
+from datetime import datetime
 
 df_of_results = pd.DataFrame({})
 
@@ -57,9 +58,9 @@ def sonuclariGoster(experiment_list):
     dateCol = df_of_results_filtered.groupby('talepID').apply(normalizeToKS)['zaman']
     dateCol = dateCol/np.timedelta64(1, 'h')
     mergedDF = df_of_results_filtered.merge(dateCol, how='inner', left_index=True, right_index=True)
-    print(dateCol)
-    print(df_of_results_filtered)
-    print(mergedDF)
+    # print(dateCol)
+    # print(df_of_results_filtered)
+    # print(mergedDF)
     graph = px.line(mergedDF, x='zaman_y', y='sonuc', text='aciklama', color='talepID')
     graph.update_traces(textposition="bottom right")
     return table_header + table_body, graph, df_of_results_filtered.to_dict()
@@ -90,10 +91,9 @@ def deneySonucuGuncelle(activated):
 )
 def downloadRawData(click, data):
     df = pd.DataFrame(data)
-    return dcc.send_data_frame(df.to_csv, "Result.csv")
+    return dcc.send_data_frame(df.to_csv, "Result_{}.csv".format(int(datetime.now().timestamp())))
 
 
 def normalizeToKS(group):
-    print(group[group['aciklama'].str.upper() == 'KS']['zaman'])
     group['zaman'] = group['zaman'] - group[group['aciklama'].str.upper() == 'KS']['zaman'].iloc[0]
     return group
